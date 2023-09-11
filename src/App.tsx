72ee4c649e7b3c057ec8e3a20892565d0ea4945e
Sizo04 <sizowork@gmail.com>
2023-09-11 16:27:29 +0200

    hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import "./App.css";
+<link
+  href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;1,700&display=swap"
+  rel="stylesheet"
+/>;
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Navbar></Navbar>
-    </>
+      <Hero></Hero>
+    </div>
   );
 }
 
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,25 @@
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <>
+      <div className="Hero-container">
+        <h4>Luxor Watches</h4>
+        <h2>Luxury Time Piece</h2>
+        <p>
+          Welcome to Luxor Watches, your one-stop shop for luxury timepieces. We
+          carry a wide selection of watches from the world's most renowned
+          brands, including Rolex, Omega, Patek Philippe, and more. We also have
+          a team of expert watchmakers who can help you find the perfect watch
+          for your needs and budget.Whether you're looking for a classic dress
+          watch, a sporty chronograph, or a diver's watch, we have the perfect
+          watch for you. We also offer a variety of services, such as watch
+          repair, customization, and watch storage.
+        </p>
+        <div className="brands"></div>
+      </div>
+    </>
+  );
+}
+
+export default Hero;
