81d50732e1ab0e8e86afcf84894bff948c4236aa
Sizo04 <sizowork@gmail.com>
2023-09-16 18:46:15 +0200

    nav&hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,22 +1,51 @@
 import "./Hero.css";
+import ap from "../Images/ap.png";
+import patek from "../Images/patek.png";
+import rolex from "../Images/rolex.png";
+import tag from "../Images/TAG_Heuer_Logo.svg.png";
+import vacheron from "../Images/vacheron.png";
+import heroImg from "../Images/luxor rolex.png";
 
 function Hero() {
   return (
     <>
-      <div className="Hero-container">
-        <h4>Luxor Watches</h4>
-        <h2>Luxury Time Piece</h2>
-        <p>
-          Welcome to Luxor Watches, your one-stop shop for luxury timepieces. We
-          carry a wide selection of watches from the world's most renowned
-          brands, including Rolex, Omega, Patek Philippe, and more. We also have
-          a team of expert watchmakers who can help you find the perfect watch
-          for your needs and budget.Whether you're looking for a classic dress
-          watch, a sporty chronograph, or a diver's watch, we have the perfect
-          watch for you. We also offer a variety of services, such as watch
-          repair, customization, and watch storage.
-        </p>
-        <div className="brands"></div>
+      <div className="hero-container">
+        <div className="text">
+          <div className="text-container">
+            <h4>Luxor Watches</h4>
+            <h2>Luxury Time Piece</h2>
+            <p>
+              Welcome to Luxor Watches, your one-stop shop for luxury
+              timepieces. We carry a wide selection of watches from the world's
+              most renowned brands, including Rolex, Omega, Patek Philippe, and
+              more. We also have a team of expert watchmakers who can help you
+              find the perfect watch for your needs and budget.Whether you're
+              looking for a classic dress watch, a sporty chronograph, or a
+              diver's watch, we have the perfect watch for you. We also offer a
+              variety of services, such as watch repair, customization, and
+              watch storage.
+            </p>
+
+            <div className="brands">
+              <button>
+                <img src={ap} alt="" />
+              </button>
+              <button>
+                <img src={patek} alt="" />
+              </button>
+              <button>
+                <img src={rolex} alt="" />
+              </button>
+              <button>
+                <img src={tag} alt="" />
+              </button>
+              <button>
+                <img src={vacheron} alt="" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="image"></div>
       </div>
     </>
   );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,42 @@
+import { useState } from "react";
+import hamMenuImg from "../Images/hamMenu.png";
 import "./Navbar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="Nav">
-      <div className="Name">
-        <h1>Luxor-Watches</h1>
+    <nav>
+      <div className="Nav">
+        <div className="Name">
+          <h1>Luxor-Watches</h1>
+        </div>
+        <div className="links">
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Watches</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+            <button
+              onClick={() => {
+                setMenuOpen(!menuOpen);
+              }}
+            >
+              <img src={hamMenuImg} alt="" />
+            </button>
+          </ul>
+        </div>
       </div>
-      <div className="links">
-        <ul>
+      <div className="hamMenu">
+        <ul className={menuOpen ? "open" : ""}>
           <li>
             <a href="">Home</a>
           </li>
@@ -22,7 +51,7 @@ function Navbar() {
           </li>
         </ul>
       </div>
-    </div>
+    </nav>
   );
 }
 
