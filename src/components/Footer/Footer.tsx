03a159c4eb5e29f06bb7d063752971a4d96474a0
unknown <sizowork@gmail.com>
2023-12-03 19:03:08 +0100

    done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,38 @@
 import "./Footer.css";
+import facebook from "../Images/facebook.png";
+import instagram from "../Images/instagram.png";
+import twitter from "../Images/twitter.png";
 
 function Footer() {
-  return <></>;
+  return (
+    <>
+      <footer>
+        <div className="footer-container">
+          <div className="icons">
+            <a href="">
+              <img src={twitter} alt="" />
+            </a>
+            <a href="">
+              <img src={instagram} alt="" />
+            </a>
+            <a href="">
+              <img src={facebook} alt="" />
+            </a>
+          </div>
+          <div className="text">
+            <a href="">Info</a>
+            <a href="">Contact</a>
+            <a href="">Location</a>
+            <div className="copy">
+              <a href="">Terms of Use</a>
+              <a href="">Privacy Policy</a>
+              <p>© Luxor Watches</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
